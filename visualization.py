9badee4d61e4a6d0957d554b219b9d529fa073e1
acroisie <arnaud.croisier@live.fr>
2025-04-25 09:53:25 +0200

    Add black formatter configuration and update visualization script for epochs processing

--- visualization.py
+++ visualization.py
@@ -15,13 +15,30 @@ from mne.decoding import CSP
 tmin, tmax = -1.0, 4.0
 event_id = dict(hands=2, feet=3)
 subject = 1
-runs = [6, 8, 10] # 6: left hand, 8: right hand, 10: feet
+runs = [6, 8, 10]  # 6: left hand, 8: right hand, 10: feet
 
 raw_fnames = eegbci.load_data(subject, runs)
 raw = concatenate_raws([read_raw_edf(f, preload=True) for f in raw_fnames])
 eegbci.standardize(raw)
-montage = make_standard_montage('standard_1005')
+montage = make_standard_montage("standard_1005")
 raw.set_montage(montage)
 
 # Filter the data
-raw.filter(7.0, 30.0, fir_design='firwin', skip_by_annotation='edge')
+raw.filter(7.0, 30.0, fir_design="firwin", skip_by_annotation="edge")
+
+events, _ = events_from_annotations(raw, event_id=dict(T1=2, T2=3))
+picks = pick_types(
+    raw.info, meg=False, eeg=True, eog=False, stim=False, exclude="bads"
+)
+
+# Read epochs
+epochs = Epochs(
+    raw,
+    events,
+    event_id,
+    tmin,
+    tmax,
+    picks=picks,
+    baseline=(None, 0),
+    preload=True,
+)
